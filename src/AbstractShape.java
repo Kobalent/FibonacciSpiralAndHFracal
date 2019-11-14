46d1199367f32b7c39c37bc9b6a6081319dac967
Kobalent <56064674+Kobalent@users.noreply.github.com>
2019-11-13 18:11:30 -0800

    Changed removeLevel(), added addLevel()
    Removed the static label from removeLevel(), moved addLevel() from HShape and removed static label

--- src/AbstractShape.java
+++ src/AbstractShape.java
@@ -18,30 +18,44 @@ public abstract class AbstractShape implements Shape{
 		return this; // Placeholder; Remove
 	}
 	
-	public static boolean removeLevel(Shape s) {
-		AbstractShape a = (AbstractShape) s;
+	public boolean addLevel() {
+		if (this.children[0] == null) {
+			// Base case
+			// Add children if the current shape has none
+			this.createChildren();
+		} else {
+			// If children array is populated, try on all the children
+			for (int i = 0; i < 7; i++) {
+				((AbstractShape) this.children[i]).addLevel();
+			}
+		}
+		this.level ++;
+		return true;
+	}
+	
+	public boolean removeLevel() {
 		
 		// cannot remove level if level = 1
-		if (a.level == 1) {
+		if (this.level == 1) {
 			return false;
 		}
 		
 		// base case
 		// if the shape's level is 2, set all the children to null and decrease the level
-		else if (a.level == 2) {
-			for (int i = 0; i< a.children.length; i++) {
-				((AbstractShape) s).children[i] = null;
+		else if (this.level == 2) {
+			for (int i = 0; i< this.children.length; i++) {
+				this.children[i] = null;
 			}
-			((AbstractShape) s).level --;
+			this.level --;
 		}
 		
 		// if the shape is any level higher than 2
 		// run the removeLevel on all its children
 		else {
-			for (int i = 0; i < a.children.length; i++) {
-				removeLevel(((AbstractShape) s).children[i]);
+			for (int i = 0; i < this.children.length; i++) {
+				((AbstractShape)this.children[i]).removeLevel();
 			}
-			((AbstractShape) s).level --;
+			this.level --;
 		}
 		
 		// return true if shape is not level 1
