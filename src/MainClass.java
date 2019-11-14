2bf77ad0ab95708b8b3c1597cb4bce27843f56dc
Kobalent <56064674+Kobalent@users.noreply.github.com>
2019-11-14 12:47:10 -0800

    Testing implemented changes
    Added addLevel calls to FibonacciSquare s to test functionality of modified code in that class

--- src/MainClass.java
+++ src/MainClass.java
@@ -12,7 +12,7 @@ public class MainClass {
 
 	private static void buildGUI() {
 		JFrame frame = new JFrame();
-		frame.setSize(800, 500);
+		frame.setSize(1500, 900);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		DrawingView panel = new DrawingView();
@@ -21,25 +21,37 @@ public class MainClass {
 		frame.setVisible(true);
 
 		DrawingModel model = new DrawingModel();
-		Shape s = new FibonacciSquare(200, 300, Color.ORANGE, 1, 7);
+		Shape s = new FibonacciSquare(200, 300, Color.ORANGE, 1, 1);
 		model.addShape(s);
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
+		System.out.println(((AbstractShape)s).addLevel());
 		model.addView(panel);
 		
 		Shape h1 = new HShape(85, 80, Color.RED, 120);
 		Shape h2 = new HShape(750, 30, Color.GREEN, 10);
 		Shape h3 = new HShape(525, 140, Color.GREEN, 15);
 		Shape h4 = new HShape(510, 260, Color.GREEN, 40);
-		model.addShape(h1);
+//		model.addShape(h1);
 		model.addShape(h2);
 		model.addShape(h3);
 		model.addShape(h4);
 		((AbstractShape)h4).addLevel();
 		((AbstractShape)h1).addLevel();
 		((AbstractShape)h1).addLevel();
+		((AbstractShape)h1).addLevel();
+		((AbstractShape)h1).addLevel();
 		System.out.println(((AbstractShape)h1).addLevel());
 		System.out.println("The level of h1 is " + ((HShape) h1).level);
 		System.out.println("The level of the sub Hs in h1 is " + ((HShape) ((HShape) h1).children[0]).level);
-		//((AbstractShape) h1).removeLevel();
+		((AbstractShape) h1).removeLevel();
 		System.out.println("The level of h1 is " + ((HShape) h1).level);
 		((AbstractShape) h2).removeLevel();
 		
