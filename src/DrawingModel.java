76addba280d65ce9e660cba52229ad6d6f3c37ef
Kobalent <56064674+Kobalent@users.noreply.github.com>
2019-11-14 22:54:13 -0800

    Final Update

--- src/DrawingModel.java
+++ src/DrawingModel.java
@@ -1,13 +1,14 @@
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DrawingModel {
-	private int state = 1;
+	public int state = 0;
 	private List<Shape> shapes = new ArrayList<Shape>();
 	private List<View> views = new ArrayList<View>();
 	
 	public boolean changeLevel(int x, int y) {
-		/*if (state == 1) {
+		if (state == 1) {
 			if (x > 750 && y < 750) {
 				return this.shapes.get(1).addLevel();
 			} else if (x > 0 && y < 750) {
@@ -20,14 +21,39 @@ public class DrawingModel {
 				return this.shapes.get(0).removeLevel();
 			}
 		}
-		return false;*/
-		shapes.get(0).addLevel();
+		return false;
+		/*shapes.get(0).addLevel();
 		shapes.get(1).addLevel();
 		
 		updateAll();
 
-		return true;
+		return true;*/
 	}
+	
+	public boolean addLevel(int x, int y) {
+			if (x > 750 && y < 750) {
+				return this.shapes.get(1).addLevel();
+			} else if (x > 0 && y < 750) {
+				return this.shapes.get(0).addLevel();
+			}
+		return false;
+	}
+	
+	public boolean removeLevel(int x, int y) {
+		if (x > 750 && y < 750) {
+			return this.shapes.get(1).removeLevel();
+		} else if (x > 0 && y < 750) {
+			return this.shapes.get(0).removeLevel();
+		}
+	return false;
+	}
+	
+	public void reset() {
+		this.shapes.set(0, new FibonacciSquare(240, 275, Color.ORANGE, 1, 1));
+		this.shapes.set(1, new HShape(800, 50, Color.RED, 200));
+		updateAll();
+	}
+	
 
 	public void addView(View v) {
 		views.add(v);
