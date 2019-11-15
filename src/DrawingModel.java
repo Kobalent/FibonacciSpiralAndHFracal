b3570755f0da87a02e6e049b0d6446814f956e70
Kobalent <56064674+Kobalent@users.noreply.github.com>
2019-11-14 16:46:42 -0800

    Added changeLevel(x,y) method
    Currently, it adds levels to both shapes, but the goal is to have the commented part be the implementation of the method

--- src/DrawingModel.java
+++ src/DrawingModel.java
@@ -5,6 +5,29 @@ public class DrawingModel {
 	private int state = 1;
 	private List<Shape> shapes = new ArrayList<Shape>();
 	private List<View> views = new ArrayList<View>();
+	
+	public boolean changeLevel(int x, int y) {
+		/*if (state == 1) {
+			if (x > 750 && y < 750) {
+				return this.shapes.get(1).addLevel();
+			} else if (x > 0 && y < 750) {
+				return this.shapes.get(0).addLevel();
+			}
+		} else {
+			if (x > 750 && y < 750) {
+				return this.shapes.get(1).removeLevel();
+			} else if (x > 0 && y < 750) {
+				return this.shapes.get(0).removeLevel();
+			}
+		}
+		return false;*/
+		shapes.get(0).addLevel();
+		shapes.get(1).addLevel();
+		
+		updateAll();
+
+		return true;
+	}
 
 	public void addView(View v) {
 		views.add(v);
